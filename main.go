c5257a6f7ef04577f865714320edaaa717b9dbe5
hudolfhess <hudolf@gmail.com>
2019-02-14 21:49:31 -0200

    simple web application

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Simple web application!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
